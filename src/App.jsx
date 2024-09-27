5c9e6bff67b82fe443953b6fe83a9d88960e2ac6
Siddhant Rathode <siddhantrathode@gmail.com>
2024-09-28 00:21:11 +0530

    input error

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ const App = () => {
 
   const [username, setusername] = useState("")
 
-  // const ChangeHandler = (e) => { 
-  //   setusername(e.target.value)
-  // }
   const SubmitHandler = (e) => {
     e.preventDefault()
-    console.log(username);
-    
+    console.log(username);  
   }
 
-
+  const [error, seterror] = useState(false)
+  // const InputHandler = (e) => {
+  //   e.target.value.length < 5 ? seterror(true) : seterror(false)
+  // }
 
   return (
-    <div className="p-5 bg-gray-600">
+    <div className="p-5 bg-gray-300">
       <form onSubmit={SubmitHandler}>
       <input className=" rounded " type="username" name="username" placeholder="username"
        onChange={(e) => setusername(e.target.value)}
+       onInput={(e) => e.target.value.length < 5 ? seterror(true) : seterror(false)}
        value={username}/>
-       <br /><br />
-      <button className="rounded bg-blue-700 p-0.5 text-white">submit</button>
+
+       {error && (
+        <p className="text-red-600">username must be minimum 5 character</p>
+       )}
+       <br />
+      <button className="rounded bg-blue-700 p-0.5 text-white mt-2">submit</button>
       </form>
     </div>
   )
